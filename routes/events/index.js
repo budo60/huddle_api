75daaf7eb808c600e619d19785efa976f9bb4f7d
moi <jbonnet60@outlook.fr>
2019-06-24 11:43:11 +0200

    fix body

--- routes/events/index.js
+++ routes/events/index.js
@@ -2,13 +2,13 @@ const data = require('express').Router();
 const event = require('./event_add');
 const event_d = require('./event_d');
 const event_e = require('./event_e');
-const event_get = require('./event_get');
+//onst event_get = require('./event_get');
 
 
 data.post('/add', event);
 data.delete(':event_id/delete', event_d);
 data.put(':event_id/save/', event_e);
-data.get('/get', event_get);
+//data.get('/get', event_get);
 
 
 
