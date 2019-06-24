e2e84642dda5e2346991366d72cbbf8a224c5134
moi <jbonnet60@outlook.fr>
2019-06-24 16:20:15 +0200

    fix route login

--- routes/users/login.js
+++ routes/users/login.js
@@ -3,21 +3,33 @@ const db = require('../../firebaseConfig');
 //console.log(db, 'ddbb');
 
 
-const log = async (login,pass) => {
-    //console.log("bla");
-    //let result="pouet";
-    db
-    .auth()
-    .signInWithEmailAndPassword(login, pass)
-    .catch( error => console.log(error.message))
-    //console.log(db.auth().currentUser)
-    return  await db.auth().currentUser
+// const log = (login,pass) => {
+//     db
+//     .auth()
+//     .signInWithEmailAndPassword(login, pass)
+//     //.then(() => {  })
+//     .catch( error => console.log(error.message))
+//     return db.auth().currentUser.uid;
+// }
+
+async function log (login,pass){
+    try{
+       await db.auth().signInWithEmailAndPassword(login,pass);
+       return db.auth().currentUser;
+    }catch(error){
+        console.log(error);
+    }
 }
 
-module.exports = (req, res) => {
-    var login = req.body.login,
-    pass = req.body.password;
 
-    //console.log(connectApp(login,pass))
-    res.send(log(login,pass))
+
+module.exports = (req, res) => {
+    return (async function(){
+        var login = req.body.login,
+        pass = req.body.password;
+        const obj = await log(login,pass);
+        res.send(obj)
+       })()
+
+    //console.log("TCL: send", send)
  };
--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
 
-axios.post('http://86.245.96.127:8002/users/login', {
+// axios.post('http://86.245.96.127:8002/users/login', {
+    axios.post('http://localhost:8002/users/login', {
     login: 'huddle@yopmail.com',
     password: 'password'
 }).then(function(res) {
-    console.log(res.data);
+    console.log("test ->>",res.data);
 }).catch(function(error){
     console.log(error)
     //reject()
