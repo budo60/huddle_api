7458234d152221ace9bc87a80c3dc5546e719231
moi <jbonnet60@outlook.fr>
2019-06-24 11:39:56 +0200

    parsing body for post

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const routes = require('./routes');
 var cors = require('cors')
 
 app.use(cors());
-app.use('/', routes); 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use('/', routes);
 
 
 app.listen(PORT, () => {
