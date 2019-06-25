8057dae5a8704b59d678696d69f4f76f12066466
lucho <pacheco.luc95@gmail.com>
2019-06-25 16:17:16 +0200

    dto added

--- routes/users/login.js
+++ routes/users/login.js
@@ -27,8 +27,27 @@ module.exports = (req, res) => {
     return (async function(){
         var login = req.body.login,
         pass = req.body.password;
-        const obj = await log(login,pass);
-        res.send(obj)
+        let obj = await log(login,pass);
+        let db_data;
+
+        let user = {
+            username: '',
+            email: '',
+            address: '',
+            phonenumber: '',
+            date: ''
+        }
+        
+        await db.database().ref('user/' + obj.uid).once('value').then((snapshot) => {
+            db_data = snapshot.val();
+            user.username = db_data.username != null ? db_data.username : obj.displayName
+            user.email = obj.email
+            user.address = db_data.address
+            user.phonenumber = db_data.phoneNumber
+            user.date = db_data.date
+        });
+
+        res.send(user)
        })()
 
     //console.log("TCL: send", send)
